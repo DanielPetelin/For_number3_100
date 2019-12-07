c4ccc28c49e1c008aa8f4bad9950111f878ac5e9
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-07 10:39:59 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-//for (var i = 0; i < 101; i++) {
-  //  if (i % 3) {
-    //console.log(i);
-   // }
-//}
-
+for (var i = 0; i < 101; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
 
 for (var j = 0; j < 101; j++) {
     if ((j % 3) == 0) {
